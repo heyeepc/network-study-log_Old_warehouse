6a64ff469ef560513d9dbf3d38b36bbafc9405e1
heyeepc <2675458141@qq.com>
2025-05-16 14:49:30 +0800

    Create R3.C

---
+++ 配置通过Telneet登录系统/R3.C
@@ -0,0 +1,9 @@
+<Huawei>sys
+Enter system view, return user view with Ctrl+Z.
+[Huawei]sysname r3
+[r3]int g0/0/0
+[r3-GigabitEthernet0/0/0]ip add 10.1.1.2 24
+[r3-GigabitEthernet0/0/0]
+May 16 2025 14:43:14-08:00 r3 %%01IFNET/4/LINK_STATE(l)[0]:The line protocol IP 
+on the interface GigabitEthernet0/0/0 has entered the UP state. 
+[r3-GigabitEthernet0/0/0]quit
