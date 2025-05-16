9bba4db2861cb15a8275160d0f94ff01f1528d4e
heyeepc <2675458141@qq.com>
2025-05-16 14:48:03 +0800

    Create R1.C

---
+++ 配置通过Telneet登录系统/R1.C
@@ -0,0 +1,11 @@
+<Huawei>sys
+Enter system view, return user view with Ctrl+Z.
+[Huawei]int g0/0/0
+[Huawei-GigabitEthernet0/0/0]ip add 10.1.1.254 24
+May 16 2025 14:41:21-08:00 Huawei %%01IFNET/4/LINK_STATE(l)[0]:The line protocol
+ IP on the interface GigabitEthernet0/0/0 has entered the UP state. 
+[Huawei-GigabitEthernet0/0/0]quit
+[Huawei]sysname r1
+[r1]user-interface vty 04
+[r1-ui-vty4]authentication-mode password
+Please configure the login password (maximum length 16):huawei
