ba227f65fb4b451481711ac44841e74b8c0b31d5
heyeepc <2675458141@qq.com>
2025-05-16 14:54:13 +0800

    Update R1.C

--- 配置通过Telneet登录系统/R1.C
+++ 配置通过Telneet登录系统/R1.C
@@ -6,6 +6,6 @@ May 16 2025 14:41:21-08:00 Huawei %%01IFNET/4/LINK_STATE(l)[0]:The line protocol
  IP on the interface GigabitEthernet0/0/0 has entered the UP state. 
 [Huawei-GigabitEthernet0/0/0]quit
 [Huawei]sysname r1
-[r1]user-interface vty 04
+[r1]user-interface vty 0 4
 [r1-ui-vty4]authentication-mode password
 Please configure the login password (maximum length 16):huawei
